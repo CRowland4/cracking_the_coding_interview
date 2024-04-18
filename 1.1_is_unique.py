3137c877ebac0df895b245490d6f56f5594f00f1
Caleb Rowland <cjrowland4@gmail.com>
2024-04-18 08:56:12 -0500

    Add another potential solution to 1.1 - Is Unique

--- 1.1_is_unique.py
+++ 1.1_is_unique.py
@@ -3,12 +3,6 @@ def is_unique(string: str) -> bool:
     return len(set(string)) == len(string)
 
 
-# Considerations: empty string, a sentence with spaces but unique letters will fail because the space is repeated
-print(is_unique("this isn't unique"))  # False
-print(is_unique("but I'm"))  # True
-print(is_unique("I am not"))  # False - because of the spaces
-
-
 # What if you can't use additional data structures?
 def is_unique2(string: str) -> bool:
     for i, char_i in enumerate(string):
@@ -19,12 +13,7 @@ def is_unique2(string: str) -> bool:
     return True
 
 
-print(is_unique("this isn't unique"))  # False
-print(is_unique("but I'm"))  # True
-print(is_unique("I am not"))  # False - because of the spaces
-
-
-# Official solution - though this technically violates the "additional data structures" constraint
+# Official solution, using other data structures
 def is_unique_optimal(string: str) -> bool:
     # Assumes that the string exclusively contains ASCII characters
     if len(string) > 128:
@@ -41,6 +30,16 @@ def is_unique_optimal(string: str) -> bool:
     return True
 
 
-print(is_unique_optimal("this isn't unique"))  # False
-print(is_unique_optimal("but I'm"))  # True
-print(is_unique_optimal("I am not"))  # False - because of the spaces
+# Another potential solution, though the sorted method produces a list, which is an additional data structure
+def is_unique_optimal2(string: str) -> bool:
+    string = "".join(sorted(string))
+    for i, char in enumerate(string[:len(string) - 1]):
+        if char == string[i + 1]:
+            return False
+
+    return True
+
+
+print(is_unique_optimal2("this isn't unique"))  # False
+print(is_unique_optimal2("but I'm"))  # True
+print(is_unique_optimal2("I am not"))  # False - because of the spaces
