68da10dcc85cc7fa74292fdebee6267c0ef5f3a6
Caleb Rowland <cjrowland4@gmail.com>
2024-04-22 08:32:54 -0500

    Add problem 2.1 - Remove Dups

---
+++ 2_1_remove_dups.py
@@ -0,0 +1,39 @@
+# Write code to remove duplicates from an unsorted linked list.
+from data_structure_classes.singly_linked_list_node import SingleNode
+
+
+def remove_dups(head: SingleNode):
+    """This solution uses a buffer"""
+    dummy = SingleNode(nxt=head)
+    prv = dummy
+    cur = head
+
+    vals = {}
+    while cur:
+        if cur.val in vals:
+            prv.next = cur.next
+        else:
+            vals.add(cur.val)
+            prv = cur
+
+        cur = cur.next
+
+    return
+
+
+def remove_dups_no_buffer(head: SingleNode):
+    """This solution doesn't use a buffer"""
+    cur = head
+
+    while cur:
+        runner = cur.next
+        while runner:
+            if cur.val == runner.val:
+                cur.next = runner.next
+
+            runner = runner.next
+
+        cur = cur.next
+
+    return
+
